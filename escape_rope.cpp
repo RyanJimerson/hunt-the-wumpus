62df4f06897d881798c4c50182f96778b9ba86eb
RyanJimerson <166050662+RyanJimerson@users.noreply.github.com>
2024-11-15 15:34:00 -0800

    Create escape_rope.cpp

---
+++ escape_rope.cpp
@@ -0,0 +1,22 @@
+#include "escape_rope.hpp"
+#include <iostream>
+
+// TODO Escape rope implementation (define escape_rope member functions below)
+void escape_rope::print(){
+	std::cout<<"E";
+}
+void escape_rope::encounter(player& p){
+
+}
+
+void escape_rope::event_location(int width, int height){
+
+}
+
+void escape_rope::is_wumpus(player& p){
+	p.is_wumpus = false;
+}
+
+void escape_rope::percept(){
+
+}
