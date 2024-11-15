4da388f47a30e0adfda1532e439a645b849e3d4b
RyanJimerson <166050662+RyanJimerson@users.noreply.github.com>
2024-11-15 15:43:30 -0800

    Create room.hpp

---
+++ room.hpp
@@ -0,0 +1,26 @@
+#ifndef ROOM_HPP
+#define ROOM_HPP
+#include <vector>
+
+#include "game.hpp"
+#include "gold.hpp"
+#include "bats.hpp"
+#include "stalactites.hpp"
+#include "wumpus.hpp"
+#include "escape_rope.hpp"
+#include "arrow.hpp"
+
+#include "event.hpp"
+
+// Room interface
+class Room {
+private: 
+	// TODO private members
+public:
+	// TODO public members
+	//Stalactites S1;
+	//std::vector < event > events {S1};//, new Stalactites, new Bats, 
+	//	new Bats, new Wumpus, new Gold, new Arrow, new Arrow, new escape_rope};
+};
+
+#endif
