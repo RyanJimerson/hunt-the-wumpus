08e0ae6ec7d4edf21a6bb54a4b8a9c3a7ec9f4e8
RyanJimerson <166050662+RyanJimerson@users.noreply.github.com>
2024-11-15 15:42:14 -0800

    Create player.hpp

---
+++ player.hpp
@@ -0,0 +1,17 @@
+#ifndef PLAYER_HPP
+#define PLAYER_HPP
+
+struct player{
+int hp = 3;
+int x_coord = 0;
+int y_coord = 0;
+bool has_gold = false;
+bool has_arrow =false;
+bool is_wumpus = false;
+bool has_armor =false;
+int armor = 0;
+bool has_flashlight=false;
+};
+
+
+#endif
